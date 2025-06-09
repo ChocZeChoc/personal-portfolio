f77f51ee9c28c61b6a275d1d5d56631e70937d16
ChocZeChoc <nathansl.littlefield@outlook.com>
2025-06-09 20:31:23 +0200

    Grid layout for portfolio

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,7 +1,54 @@
+import './Portfolio.css'
+import pototo from '/src/assets/img/Pototo.jpg'
+
 const Home = () => {
   return (
     <div>
-      <h1>Portfolio</h1>
+        <div>
+            <h1>Portfolio</h1>
+        </div>
+        <div className="grid-container">
+            <div className="block">
+                <img src={pototo} width={'500px'}/>
+                <h2>Project 1</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis harum sunt dicta molestias repellat iusto cupiditate reiciendis ut aperiam rem consequatur, voluptates fugit, sequi corporis? Veniam fuga vel eligendi facere.</p>
+            </div>
+            <div className="block">
+                <img src={pototo} width={'500px'}/>
+                <h2>Project 2</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis harum sunt dicta molestias repellat iusto cupiditate reiciendis ut aperiam rem consequatur, voluptates fugit, sequi corporis? Veniam fuga vel eligendi facere.</p>
+            </div>
+            <div className="block">
+                <img src={pototo} width={'500px'}/>
+                <h2>Project 3</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis harum sunt dicta molestias repellat iusto cupiditate reiciendis ut aperiam rem consequatur, voluptates fugit, sequi corporis? Veniam fuga vel eligendi facere.</p>
+            </div>
+            <div className="block">
+                <img src={pototo} width={'500px'}/>
+                <h2>Project 4</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis harum sunt dicta molestias repellat iusto cupiditate reiciendis ut aperiam rem consequatur, voluptates fugit, sequi corporis? Veniam fuga vel eligendi facere.</p>
+            </div>
+            <div className="block">
+                <img src={pototo} width={'500px'}/>
+                <h2>Project 5</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis harum sunt dicta molestias repellat iusto cupiditate reiciendis ut aperiam rem consequatur, voluptates fugit, sequi corporis? Veniam fuga vel eligendi facere.</p>
+            </div>
+            <div className="block">
+                <img src={pototo} width={'500px'}/>
+                <h2>Project 6</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis harum sunt dicta molestias repellat iusto cupiditate reiciendis ut aperiam rem consequatur, voluptates fugit, sequi corporis? Veniam fuga vel eligendi facere.</p>
+            </div>
+            <div className="block">
+                <img src={pototo} width={'500px'}/>
+                <h2>Project 7</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis harum sunt dicta molestias repellat iusto cupiditate reiciendis ut aperiam rem consequatur, voluptates fugit, sequi corporis? Veniam fuga vel eligendi facere.</p>
+            </div>
+            <div className="block">
+                <img src={pototo} width={'500px'}/>
+                <h2>Project 8</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis harum sunt dicta molestias repellat iusto cupiditate reiciendis ut aperiam rem consequatur, voluptates fugit, sequi corporis? Veniam fuga vel eligendi facere.</p>
+            </div>
+        </div>
     </div>
   );
 };
