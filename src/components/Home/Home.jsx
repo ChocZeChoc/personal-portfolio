83bf014b8d1db0fbc9d5388c21183913486c6de4
ChocZeChoc <nathansl.littlefield@outlook.com>
2025-06-09 19:04:34 +0200

    Add Homepage stuff

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,19 @@
 import './Home.css'
+import profile from '/src/assets/img/Linkedin.jpg'
 
 const Home = () => {
   return (
+    <div className='homePage'>
     <div>
-        
-      <h1>Hello I'm Nathan</h1>
-      <h2>Cat Lover</h2>
-      <p>Bam</p>
+        <img src={profile} alt='profile pic' style={{maxHeight:'500px'}} className='profileimg'/>
+    </div>
+    <div className='homeIntro'>
+        <h1>Hello I'm <span style={{color:'#6600cc'}}>Nathan</span></h1> 
+        <h2>Cat Lover & Beetle Breeder</h2>
+      <span className='homeText'>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate, quibusdam! Explicabo, similique! Magni qui obcaecati cumque at porro eius corporis! Illum recusandae alias dignissimos perspiciatis nesciunt iusto provident odio sed sapiente blanditiis sit, suscipit doloremque voluptatem distinctio obcaecati totam, ad quod ex. Assumenda rerum odio qui. Dolorem et aspernatur rerum, dolorum doloremque cupiditate. Iste ut natus consequuntur aperiam autem voluptate debitis animi iure quae, est placeat facilis accusantium similique dicta. Dicta sunt labore in at laborum quam tenetur error exercitationem deleniti laudantium, magni corrupti molestias incidunt repudiandae voluptatibus ab aspernatur quibusdam eligendi explicabo aperiam quisquam. Nihil a sint consectetur dignissimos.</p>
+    </span>
+    </div>
     </div>
   );
 };
