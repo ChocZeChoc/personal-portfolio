cdb6edae0240f9ee0116adf9e2c1fe30297b5529
ChocZeChoc <nathansl.littlefield@outlook.com>
2025-06-10 17:26:59 +0200

    align portfolio

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -19,50 +19,108 @@ const Home = () => {
         </div>
         <div className="grid-container">
             <div className="block">
-                <img src={BeetleBreeder} width={'460px'}/>
+                <img src={BeetleBreeder} width={'460px'} height={'460px'}/>
                 <h2>Beetle Breeder</h2>
                 <p>Experience evolution in action with this Unity-powered simulation! Watch AI creatures compete, adapt, and evolve through natural selection. Traits like speed and vision affect survival, while genetic algorithms introduce mutations. Beginner-friendly and dynamic, it’s a fun, interactive way to explore evolution, genetics, and emergent behavior in a living digital world.</p>
+                <div className='linkbutton'>
                 <button onClick={() => handleClick(project_BB_Github)}>
                     Go to Repository
                 </button>
                 <button onClick={() => handleClick(project_BB_itch)}>
                     Go to itch.io
                 </button>
+                </div>
             </div>
             <div className="block">
-                <img src={pototo} width={'460px'}/>
+                <img src={pototo} width={'460px'} height={'460px'}/>
                 <h2>Project 2</h2>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis harum sunt dicta molestias repellat iusto cupiditate reiciendis ut aperiam rem consequatur, voluptates fugit, sequi corporis? Veniam fuga vel eligendi facere.</p>
+                <div className='linkbutton'>
+                <button onClick={() => handleClick(project_BB_Github)}>
+                    Go to Repository
+                </button>
+                <button onClick={() => handleClick(project_BB_itch)}>
+                    Go to itch.io
+                </button>
+                </div>
             </div>
             <div className="block">
-                <img src={pototo} width={'460px'}/>
+                <img src={pototo} width={'460px'} height={'460px'}/>
                 <h2>Project 3</h2>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis harum sunt dicta molestias repellat iusto cupiditate reiciendis ut aperiam rem consequatur, voluptates fugit, sequi corporis? Veniam fuga vel eligendi facere.</p>
+                <p>Lere.</p>
+                <div className='linkbutton'>
+                <button onClick={() => handleClick(project_BB_Github)}>
+                    Go to Repository
+                </button>
+                <button onClick={() => handleClick(project_BB_itch)}>
+                    Go to itch.io
+                </button>
+                </div>
             </div>
             <div className="block">
-                <img src={pototo} width={'460px'}/>
+                <img src={pototo} width={'460px'} height={'460px'}/>
                 <h2>Project 4</h2>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis harum sunt dicta molestias repellat iusto cupiditate reiciendis ut aperiam rem consequatur, voluptates fugit, sequi corporis? Veniam fuga vel eligendi facere.</p>
+                <div className='linkbutton'>
+                <button onClick={() => handleClick(project_BB_Github)}>
+                    Go to Repository
+                </button>
+                <button onClick={() => handleClick(project_BB_itch)}>
+                    Go to itch.io
+                </button>
+                </div>
             </div>
             <div className="block">
-                <img src={pototo} width={'460px'}/>
+                <img src={pototo} width={'460px'} height={'460px'}/>
                 <h2>Project 5</h2>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis harum sunt dicta molestias repellat iusto cupiditate reiciendis ut aperiam rem consequatur, voluptates fugit, sequi corporis? Veniam fuga vel eligendi facere.</p>
+                <div className='linkbutton'>
+                <button onClick={() => handleClick(project_BB_Github)}>
+                    Go to Repository
+                </button>
+                <button onClick={() => handleClick(project_BB_itch)}>
+                    Go to itch.io
+                </button>
+                </div>
             </div>
             <div className="block">
-                <img src={pototo} width={'460px'}/>
+                <img src={pototo} width={'460px'} height={'460px'}/>
                 <h2>Project 6</h2>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis harum sunt dicta molestias repellat iusto cupiditate reiciendis ut aperiam rem consequatur, voluptates fugit, sequi corporis? Veniam fuga vel eligendi facere.</p>
+                <div className='linkbutton'>
+                <button onClick={() => handleClick(project_BB_Github)}>
+                    Go to Repository
+                </button>
+                <button onClick={() => handleClick(project_BB_itch)}>
+                    Go to itch.io
+                </button>
+                </div>
             </div>
             <div className="block">
-                <img src={pototo} width={'460px'}/>
+                <img src={pototo} width={'460px'} height={'460px'}/>
                 <h2>Project 7</h2>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis harum sunt dicta molestias repellat iusto cupiditate reiciendis ut aperiam rem consequatur, voluptates fugit, sequi corporis? Veniam fuga vel eligendi facere.</p>
+                <div className='linkbutton'>
+                <button onClick={() => handleClick(project_BB_Github)}>
+                    Go to Repository
+                </button>
+                <button onClick={() => handleClick(project_BB_itch)}>
+                    Go to itch.io
+                </button>
+                </div>
             </div>
             <div className="block">
-                <img src={pototo} width={'460px'}/>
+                <img src={pototo} width={'460px'} height={'460px'}/>
                 <h2>Project 8</h2>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis harum sunt dicta molestias repellat iusto cupiditate reiciendis ut aperiam rem consequatur, voluptates fugit, sequi corporis? Veniam fuga vel eligendi facere.</p>
+                <div className='linkbutton'>
+                <button onClick={() => handleClick(project_BB_Github)}>
+                    Go to Repository
+                </button>
+                <button onClick={() => handleClick(project_BB_itch)}>
+                    Go to itch.io
+                </button>
+                </div>
             </div>
         </div>
     </div>
